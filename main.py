b8f3e4ebf1cade892f7e9ee0aafc950658ba4387
jabuta <lfalvaz@gmail.com>
2023-12-24 00:54:09 -0500

    start making funciton

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 import requests
-import search_engines
+from search_engines import search_engines
+from key import key
+
+
+api_url = "https://www.googleapis.com/customsearch/v1?"
+q = "Que son las obras por impuestos colombia"
+parameters = {"key": key, "cx": search_engines["es"]["col"],"q": q}
+
+
+
+x = requests.get(api_url,parameters)
+
+x = x.json()
+
+resultlist = []
+for item in x["items"]:
+    resultlist.append(item["link"])
+
+print(resultlist)
