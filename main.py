13d825273b82947f0421bfaada91f88efa9ec6dc
jabuta <lfalvaz@gmail.com>
2023-12-24 00:55:37 -0500

    start making funciton

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from search_engines import search_engines
 from key import key
 
 
-api_url = "https://www.googleapis.com/customsearch/v1?"
+
 q = "Que son las obras por impuestos colombia"
 parameters = {"key": key, "cx": search_engines["es"]["col"],"q": q}
 
@@ -18,3 +18,7 @@ for item in x["items"]:
     resultlist.append(item["link"])
 
 print(resultlist)
+
+
+def get_results(q,search_engines):
+    api_url = "https://www.googleapis.com/customsearch/v1?"
