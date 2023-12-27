5ba3901be33ea6fcb3953a368ddf9704c0a549d6
jabuta <lfalvaz@gmail.com>
2023-12-27 11:26:42 -0500

    fix function separation

--- get_google_results.py
+++ get_google_results.py
@@ -1,13 +1,12 @@
 import requests
-from search_engines import search_engines
 from key import key
 
 
 
 def get_google_results(query,parameters):
+    parameters["key"] = key
+    parameters["q"] = query
     
-    parameters = {"key": key, "cx": search_engines["es"]["col"],"q": query}
-
     api_url = "https://www.googleapis.com/customsearch/v1?"
 
     try:
--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-import requests
+from get_google_results import get_google_results
 from search_engines import search_engines
-from key import key
 
 
-
-q = "Que son las obras por impuestos colombia"
-parameters = {"key": key, "cx": search_engines["es"]["col"],"q": q}
+parameters = {"cx": search_engines["es"]["col"]}
 
 
+def main():
+    print(get_google_results("Obras por impuestos",parameters))
 
-x = requests.get(api_url,parameters)
-
-x = x.json()
-
-resultlist = []
-for item in x["items"]:
-    resultlist.append(item["link"])
-
-print(resultlist)
-
-
-def get_results(q,search_engines):
-    api_url = "https://www.googleapis.com/customsearch/v1?"
+main()
