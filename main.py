9c7746f600ccc68065e8993fbf35bb8ba7cb8d93
jabuta <lfalvaz@gmail.com>
2024-01-09 21:22:58 -0500

    some message

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from get_google_results import get_google_results
 from search_engines import search_engines
-
+import csv
 
 parameters = {"cx": search_engines["es"]["col"]}
+
+keywords = {}
+
+with open("workspace/github.com/jabuta/rank-tracker/files/keywords.csv",) as csvkeywords:
+    kwdsdata = list(csv.reader(csvkeywords))
+    for kwd in kwdsdata:
+        keywords[kwd[0]] = kwd[1:]
+print(keywords)
+
 
 
 def main():
     print(get_google_results("Obras por impuestos",parameters))
 
-main()
+# main()
